68ec5d3c7627367ff17e1f10bd6677cdcafa7093
SimplyKyra <SimplyKyra@users.noreply.github.com>
2023-12-30 10:54:19 -0800

    Initial Commit

---
+++ multiselectionPickerSwiftData/multiselectionPickerSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiselectionPickerSwiftDataApp.swift
+//  multiselectionPickerSwiftData
+//
+//  Created by Kyra Delaney on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct multiselectionPickerSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
